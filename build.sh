0d611946a39c0c5bda4de810f1af5fff0cbf91e2
Henrik Urlund <henrik@urlund.com>
2017-02-07 13:30:21 +0100

    added build script

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+VERSION=`cat VERSION`
+GOOS=linux
+GOARCH=amd64
+BIN_NAME="nginx-mail-auth-http"
+BIN_PATH="bin/$BIN_NAME-$GOOS-$GOARCH-$VERSION"
+GIT_REPO="github.com/urlund/$BIN_NAME"
+
+rm -rf $BIN_NAME
+
+sed -i '' -e "s/\"version: .*\"/\"version: $VERSION\"/g" init.go
+
+# check if docker is installed
+if [ $(which docker > /dev/null 2>&1; echo $?) -ne 0 ]; then
+    echo "you must have docker installed to use this script"
+    exit 1;
+fi
+
+# check if $GOPATH isset
+if [ -z $GOPATH ]; then
+    echo "you must set \$GOPATH to use this script"
+    exit 1;
+fi
+
+# run docker with build cmd
+docker run --rm -it -v "$GOPATH":/work -e "GOPATH=/work" -w /work/src/$GIT_REPO -e GOOS=$GOOS -e GOARCH=$GOARCH golang:latest go build -o $BIN_PATH
